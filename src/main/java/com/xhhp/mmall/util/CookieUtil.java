8d942e882ddefbfaf40183da0119ebb1f91bfd3b
819163324@qq.com <819163324@qq.com>
2019-10-13 22:19:08 +0800

    10.13 commit

--- src/main/java/com/xhhp/mmall/util/CookieUtil.java
+++ src/main/java/com/xhhp/mmall/util/CookieUtil.java
@@ -1,125 +1,57 @@
-package com.xhhp.mmall.controller.portal;
+package com.xhhp.mmall.util;
 
-import com.xhhp.mmall.common.Const;
-import com.xhhp.mmall.common.JsonUtil;
-import com.xhhp.mmall.common.ResponseCode;
-import com.xhhp.mmall.common.ServerResponse;
-import com.xhhp.mmall.pojo.User;
-import com.xhhp.mmall.service.IUserSerivce;
-import com.xhhp.mmall.util.CookieUtil;
-import com.xhhp.mmall.util.RedisPoolUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * UserController class
+ * CookieUtil class
  *
  * @author Flc
- * @date 2019/9/25
+ * @date 2019/10/9
  */
-@Controller
-@RequestMapping("/user/")
-public class UserController {
+public class   CookieUtil {
+    private final static String COOKIE_DOMAIN = ".localhost";
+    private final static String COOKIE_NAME = "mmal_login_token";
 
-    @Autowired
-    private IUserSerivce iUserSerivce;
-
-    /**
-     * 用户登录功能
-     * @param username
-     * @param password
-     * @param session
-     * @return
-     */
-    @RequestMapping(value = "login.do", method = RequestMethod.GET)
-    @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse servletResponse, HttpServletRequest request) {
-        ServerResponse<User> response = iUserSerivce.login(username,password);
-
-        if(response.isSuccess()){
-            CookieUtil.readLoginToken(request);
-            RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
-
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie:cookies) {
+                if(StringUtils.equals(cookie.getName(),COOKIE_NAME)) {
+                    return cookie.getValue();
+                }
+            }
         }
-        return response;
+        return null;
     }
 
 
-    @RequestMapping(value = "logout.do" , method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse logOut(HttpServletRequest request, HttpServletResponse response) {
-//        session.removeAttribute(Const.CurrentUser);
-        String loginToken = CookieUtil.readLoginToken(request);
-        CookieUtil.delLoginToken(request, response);
-        RedisPoolUtil.del(loginToken);
-        return ServerResponse.createBySuccess();
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie ck = new Cookie(COOKIE_NAME, token);
+        ck.setDomain(COOKIE_DOMAIN);
+        ck.setPath("/");//设置在根目录
+        //-1代表永久
+        ck.setMaxAge(60*60*24*365);
+        ck.setHttpOnly(true);
+        response.addCookie(ck);
     }
 
-    @RequestMapping(value = "register.do" , method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<String> register(@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("email") String email,@RequestParam("phone") String phone,@RequestParam("question") String question,@RequestParam("answer") String answer) {
-        User user = new User(username,password,email,phone,question,answer);
-        return iUserSerivce.register(user);
-    }
 
-    @RequestMapping(value = "check_valid.do" , method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<String> checkValid(@RequestParam("str") String str,@RequestParam("type") String type) {
-        return iUserSerivce.checkValid(str, type);
-    }
-
-    @RequestMapping(value = "get_user_info.do" ,method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<User> getUserInfo( HttpServletRequest request) {
-//        User user = (User)session.getAttribute(Const.CurrentUser);
-//        System.out.println(user);
-        String loginToken = CookieUtil.readLoginToken(request);
-        if(StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByERRORMessage("用户未登录，无法获取信息");
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie:cookies) {
+                if(StringUtils.equals(cookie.getName(),COOKIE_NAME)) {
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
         }
-        String userJsonStr = RedisPoolUtil.get(loginToken);
-        User user = JsonUtil.string2Obj(userJsonStr,User.class);
-        if(user != null) {
-            return ServerResponse.createBySuccess(user);
-        }
-        return ServerResponse.createByERRORMessage("用户未登录，无法获取信息");
-    }
-
-    @RequestMapping(value = "forget_get_question.do", method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<String> forgetGetQuestion(@RequestParam("username") String username){
-        return iUserSerivce.selctQuestion(username);
-    }
-
-    @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<String> forgetCheckQuestion(@RequestParam("username")String username, @RequestParam("question")String question, @RequestParam("answer")String answer) {
-        return iUserSerivce.checkAnswer(username,question,answer);
-    }
-
-
-    @RequestMapping(value = "forget_reset_password.do", method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<String> forgetRestPassword(@RequestParam("username") String username, @RequestParam("passwordNew") String passwordNew,@RequestParam("forgetToken") String forgetToken) {
-        return iUserSerivce.forgetResetPassword(username,passwordNew,forgetToken);
-    }
-
-
-    @RequestMapping(value = "get_information.do", method = RequestMethod.POST)
-    @ResponseBody
-    public ServerResponse<User> getInformation(HttpSession session) {
-
-        User currentUser = (User)session.getAttribute(Const.CurrentUser);
-        if(currentUser == null) {
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录，需要强制登录");
-        }
-        return iUserSerivce.getInformation(currentUser.getId());
     }
 }
