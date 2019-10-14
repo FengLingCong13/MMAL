3f98320dc703f71ce5bd987213b77aa2e92e3e36
819163324@qq.com <819163324@qq.com>
2019-10-14 09:10:14 +0800

    add session filter

---
+++ src/main/java/com/xhhp/mmall/controller/backend/common/SessionExpireFilter.java
@@ -0,0 +1,44 @@
+package com.xhhp.mmall.controller.backend.common;
+
+import com.xhhp.mmall.common.Const;
+import com.xhhp.mmall.common.JsonUtil;
+import com.xhhp.mmall.common.RedisPool;
+import com.xhhp.mmall.pojo.User;
+import com.xhhp.mmall.util.CookieUtil;
+import com.xhhp.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * SessionExpireFilter class
+ *
+ * @author Flc
+ * @date 2019/10/14
+ */
+@Component
+@WebFilter(urlPatterns = "*.do", filterName = "SessionExpireFilter")
+public class SessionExpireFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+
+        if(StringUtils.isEmpty(loginToken)) {
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr, User.class);
+
+            if(user != null) {
+                //重置session的时间
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
